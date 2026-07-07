66efa917278504d13a3efc37fc9fead72afc9367
SahilDave04 <sahildave04@gmail.com>
2026-07-08 00:27:32 +0530

    Problems solved on 7 July 2026

---
+++ Daily-Temperatures.py
@@ -0,0 +1,18 @@
+﻿class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        stack = list()
+        ans = [0]*len(temperatures)
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                sTemp,sInd = stack.pop()
+                ans[sInd] = i - sInd
+            stack.append([t,i])
+        return ans
+
+
+temperatures = [73,74,75,71,69,72,76,73]
+worker = Solution()
+ans1 = worker.dailyTemperatures(temperatures)
+print(ans1)
+
+#Daily Temperatures
