812e58bdeed4317e2a65e769c0b3e7a98ddfc98a
SahilDave04 <sahildave04@gmail.com>
2026-08-01 00:52:03 +0530

    Problems solved on 31 July 2026

---
+++ Find-in-Mountain-Array.py
@@ -0,0 +1,72 @@
+﻿import math
+
+class MountainArray:
+    def __init__(self,mountainArr):
+        self.mountainArr = mountainArr
+
+    def get(self, index: int) -> int:
+        return self.mountainArr[index]
+
+    def length(self) -> int:
+        return len(self.mountainArr)
+
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        l,r = 1,mountainArr.length()-2
+        count = 1
+
+        #Finding Peak
+        while True:
+            m = (l+r)//2
+            mVal = mountainArr.get(m)
+            nextM = mountainArr.get(m+1)
+            if mVal < nextM:
+                l = m + 1
+            elif mVal > nextM:
+                if mVal > mountainArr.get(m-1):
+                    break
+                r = m - 1
+            count += 2
+        
+        #Binary Search on Left Side
+        l,r = 0,m
+        res = -1
+        while l <= r:
+            m = (l+r)//2
+            mVal = mountainArr.get(m)
+            count += 1
+            #print(res)
+            if target < mVal:
+                r = m -1
+            elif target > mVal:
+                l = m + 1
+            else:
+                print(count)
+                return m
+
+        #Binary Search on Right Side
+        l,r = m+1,mountainArr.length()-1      
+        while l <= r:
+            m = (l+r)//2
+            mVal = mountainArr.get(m)   
+            count += 1
+            #print(res)
+            if target > mVal:
+                r = m -1
+            elif target < mVal:
+                l = m + 1
+            else:
+                print(count)
+                return m
+        print(count)
+        return -1
+
+      
+mountainArr = [1,2,3,4,5,3,1]
+target = 3
+mountain = MountainArray(mountainArr)
+worker = Solution()
+ans1 = worker.findInMountainArray(target,mountain)
+print(ans1)
+
+#Find in Mountain Array
---
+++ Reverse-Linked-List-(Important-Question).py
@@ -0,0 +1,40 @@
+﻿import math
+
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        def recursive(head):
+            if not head:
+                return None
+
+            newHead = head
+            while head.next:
+                print(head)
+                newHead = recursive(head.next)
+                head.next.next = head
+            head.next = None
+
+            return newHead
+
+        def iterative(head):
+            prev,curr = None, head
+
+            while curr:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            return prev
+
+        return recursive(head),iterative(head)
+        
+head = [1,2,3,4,5]
+worker = Solution()
+ans1 = worker.reverseList(head)
+print(ans1)
+
+#Reverse Linked List (Important Question)
