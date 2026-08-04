27dca7f018445fbcc7f7f3ed88a9113b62f0f9e6
SahilDave04 <sahildave04@gmail.com>
2026-08-04 23:17:51 +0530

    Problems solved on 4 August 2026

---
+++ Find-the-Duplicate-Number-(Very-Important-Question).py
@@ -0,0 +1,27 @@
+﻿class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+
+        # Floyd's Cycle Detection (watch video again)
+        # Slow Pointer (one step), Fast Pointer (two steps)
+
+        slow1, fast = 0,0
+        while True:
+            slow1 = nums[slow1]
+            fast = nums[nums[fast]]
+            if slow1 == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow1 = nums[slow1]
+            slow2 = nums[slow2]
+            if slow1 == slow2:
+                return slow1
+        
+                
+nums = [1,3,4,3,2]
+worker = Solution()
+ans1 = worker.findDuplicate((nums))
+print(ans1)
+
+#Find the Duplicate Number (Very Important Question)
---
+++ Reverse-Linked-List-II-(Important-Question).py
@@ -0,0 +1,34 @@
+﻿from typing import Optional
+from utilities import ListNode, createLinks, printLinks
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(next=head)
+        l1,cur = dummy,head
+
+        # This Loop runs till we reach Left, l1-> node before Left, cur-> Left
+        for i in range(left-1):
+            l1,cur = cur, cur.next
+
+        # This Loops run till we reach Right, prev-> Right, cur-> node after Right
+        prev = None
+        for i in range(right-left+1):
+            temp = cur.next
+            cur.next = prev
+            prev, cur = cur, temp
+
+        # Setting Pointers, l1->right, left->cur
+        l1.next.next = cur
+        l1.next = prev
+        
+        return dummy.next
+        
+                
+head = [5]
+left = 1
+right = 1
+worker = Solution()
+ans1 = worker.reverseBetween(createLinks(head),left,right)
+print(ans1)
+
+#Reverse Linked List II (Important Question)
