a10e8aff213eb63912b62980c2119bf432cf1199
SahilDave04 <sahildave04@gmail.com>
2026-08-19 23:49:09 +0530

    Problems solved on 19 August 2026

---
+++ Binary-Tree-Inorder-Traversal-(Important-Question).py
@@ -0,0 +1,40 @@
+﻿from utilities import TreeNode, only_function, createBinaryTree, printLinks
+
+class Solution:
+    def inorderTraversal(self, root: [TreeNode]) -> list[int]:
+        def iterativeSol():
+            res = []
+            stack = []
+            cur = root
+
+            while cur or stack:
+                while cur:
+                    stack.append(cur)
+                    cur = cur.left
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right
+
+            return res
+
+        def recursiveSol():
+            res = []
+
+            def inorder(root):
+                if not root:
+                    return
+                inorder(root.left)
+                res.append(root.val)
+                inorder(root.right)
+
+            inorder(root)
+            return res
+        
+        return recursiveSol(), iterativeSol()
+
+root = [1,None,2,3]
+output = only_function("inorderTraversal", createBinaryTree(root))
+print(output)
+
+
+#Binary Tree Inorder Traversal (Important Question)
